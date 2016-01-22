8ace83ef6a572e85a3989f1691e6f6ec9895a433
unknown <serhii.kokovskyi@LWO1-DHP-F53476.synapse.com>
2016-01-22 19:52:58 +0200

    work copy

--- angular/customPhonecat/app/js/controllers.js
+++ angular/customPhonecat/app/js/controllers.js
@@ -5,18 +5,13 @@
 var app = angular.module('phonecatApp', []);
 
 //  The controller is simply a constructor function that takes a $scope parameter:
-app.controller('PhoneListCtrl', function($scope) {
-    $scope.phones = [
-        {'name': 'Nexus S',
-            'snippet': 'Fast just got faster with Nexus S.',
-            'age': 1},
-        {'name': 'Motorola XOOM™ with Wi-Fi',
-            'snippet': 'The Next, Next Generation tablet.',
-            'age': 2},
-        {'name': 'MOTOROLA XOOM™',
-            'snippet': 'The Next, Next Generation tablet.',
-            'age': 3}
-    ];
+app.controller('PhoneListCtrl', ['$scope', '$http',
+    function($scope, $http) {
 
-    $scope.orderProp = 'age';
-});
+        $http.get('phones/phones.json').success(function(data) {
+            $scope.phones = data;
+        });
+
+        $scope.orderProp = 'age';
+    }
+]);
